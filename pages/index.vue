b0ca40f9d185baae6dfd6dd6b95b1d41d5bea614
Your Name <you@example.com>
2020-07-08 10:31:50 +0500

    remove dist

--- pages/index.vue
+++ pages/index.vue
@@ -11,14 +11,14 @@
 import slideshow from '@/components/home/slideshow.vue'
 import content_1 from '@/components/home/content-1.vue'
 import content_2 from '@/components/home/content-2.vue'
-import img1 from '@/assets/images/1.jpg'
-import img2 from '@/assets/images/2.jpg'
-import img3 from '@/assets/images/3.jpg'
-import img4 from '@/assets/images/4.jpg'
-import img5 from '@/assets/images/5.jpg'
-import img6 from '@/assets/images/6.jpg'
-import img7 from '@/assets/images/7.jpg'
-import img8 from '@/assets/images/8.jpg'
+import img1 from '../assets/images/1.jpg'
+import img2 from '../assets/images/2.jpg'
+import img3 from '../assets/images/3.jpg'
+import img4 from '../assets/images/4.jpg'
+import img5 from '../assets/images/5.jpg'
+import img6 from '../assets/images/6.jpg'
+import img7 from '../assets/images/7.jpg'
+import img8 from '../assets/images/8.jpg'
 export default {
   components: {
     "slideshow": slideshow,
